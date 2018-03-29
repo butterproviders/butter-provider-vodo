a9a41ca1538ba7e4a80b09311190f4d8e4e58590
Niv Sardi <xaiki@evilgiggle.com>
2018-03-29 04:08:30 -0300

    migrate to ES6
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -4,23 +4,12 @@ var Provider = require('butter-provider');
 var querystring = require('querystring');
 var Q = require('q');
 var axios = require('axios');
-var inherits = require('util').inherits;
 var _ = require('lodash');
 var Datastore = require('nedb');
 
 var db = new Datastore();
 
-function Vodo(args) {
-    if (!(this instanceof Vodo)) {
-        return new Vodo(args);
-    }
-
-    Provider.call(this, args);
-    this.apiUrl = this.args.urlList
-}
-inherits(Vodo, Provider);
-
-Vodo.prototype.config = {
+const defaultConfig = {
     name: 'vodo',
     uniqueId: 'imdb_id',
     tabName: 'Vodo',
@@ -36,20 +25,20 @@ Vodo.prototype.config = {
     defaults: {
         urlList: ['http://butter.vodo.net/popcorn']
     },
-    args: {
+    argTypes: {
         urlList: Provider.ArgType.ARRAY
     },
     /* should be removed */
     //subtitle: 'ysubs',
     metadata: 'trakttv:movie-metadata'
-};
+}
 
 function formatForButter(items) {
     var results = {};
     var movieFetch = {};
     movieFetch.results = [];
     movieFetch.hasMore = (Number(items.length) > 1 ? true : false);
-    items.forEach(function (movie) {
+    items.forEach((movie) => {
         if (movie.Quality === '3D') {
             return;
         }
@@ -97,95 +86,100 @@ function formatForButter(items) {
     return movieFetch.results;
 }
 
-Vodo.prototype.updateAPI = function () {
-    var self = this;
-    var defer = Q.defer();
-    console.info('Request to Vodo', this.apiUrl);
-    axios(this.apiUrl[0], {
-        strictSSL: false,
-        json: true,
-        timeout: 10000
-    })
-    .then(function(res) {
-        let data = res.data
-        /*
-           data = _.map (helpers.formatForButter(data), function (item) {
-           item.rating = item.rating.percentage * Math.log(item.rating.votes);
-           return item;
-           });
-         */
-        db.insert(formatForButter(data.downloads), function (err, newDocs) {
-            if (err) {
-                console.error('Vodo.updateAPI(): Error inserting', err);
-            }
+module.exports = class Vodo extends Provider {
+    constructor (args, config = defaultConfig) {
+        super(args, config)
 
-            db.find({}).limit(2).exec(function (err, docs) {
-                //console.debug('FIND ---->', err, docs);
-            });
-            defer.resolve(newDocs);
-        });
-    })
-
-    return defer.promise;
-};
-
-Vodo.prototype.fetch = function (filters) {
-    var self = this;
-    if (!self.fetchPromise) {
-        self.fetchPromise = this.updateAPI();
+        this.apiUrl = this.args.urlList
     }
 
-    var defer = Q.defer();
-    var params = {
-        sort: 'rating',
-        limit: 50
-    };
-    var findOpts = {};
+    updateAPI() {
+        var self = this;
+        var defer = Q.defer();
+        console.info('Request to Vodo', this.apiUrl);
+        axios(this.apiUrl[0], {
+            strictSSL: false,
+            json: true,
+            timeout: 10000
+        })
+            .then((res) => {
+                let data = res.data
+                /*
+                   data = _.map (helpers.formatForButter(data), (item) => {
+                   item.rating = item.rating.percentage * Math.log(item.rating.votes);
+                   return item;
+                   });
+                 */
+                db.insert(formatForButter(data.downloads), (err, newDocs) => {
+                    if (err) {
+                        console.error('Vodo.updateAPI(): Error inserting', err);
+                    }
 
-    if (filters.keywords) {
-        findOpts = {
-            title: new RegExp(filters.keywords.replace(/\s/g, '\\s+'), 'gi')
+                    db.find({}).limit(2).exec((err, docs) => {
+                        //console.debug('FIND ---->', err, docs);
+                    });
+                    defer.resolve(newDocs);
+                });
+            })
+
+        return defer.promise;
+    }
+    fetch(filters = {}) {
+        var self = this;
+        if (!self.fetchPromise) {
+            self.fetchPromise = this.updateAPI();
+        }
+
+        var defer = Q.defer();
+        var params = {
+            sort: 'rating',
+            limit: 50
         };
+        var findOpts = {};
+
+        if (filters.keywords) {
+            findOpts = {
+                title: new RegExp(filters.keywords.replace(/\s/g, '\\s+'), 'gi')
+            };
+        }
+
+        if (filters.genre) {
+            params.genre = filters.genre;
+        }
+
+        if (filters.order) {
+            params.order = filters.order;
+        }
+
+        if (filters.sorter && filters.sorter !== 'popularity') {
+            params.sort = filters.sorter;
+        }
+
+        var sortOpts = {};
+        sortOpts[params.sort] = params.order;
+
+        self.fetchPromise.then(() => {
+            db.find(findOpts)
+              .sort(sortOpts)
+              .skip((filters.page - 1) * params.limit)
+              .limit(Number(params.limit))
+              .exec((err, docs) => {
+                  docs.forEach((entry) => {
+                      entry.type = 'movie';
+                  });
+
+                  return defer.resolve({
+                      results: docs,
+                      hasMore: docs.length ? true : false
+                  });
+              });
+        });
+
+        return defer.promise;
     }
 
-    if (filters.genre) {
-        params.genre = filters.genre;
+    detail (torrent_id, old_data) {
+        return Q(old_data);
     }
+}
 
-    if (filters.order) {
-        params.order = filters.order;
-    }
-
-    if (filters.sorter && filters.sorter !== 'popularity') {
-        params.sort = filters.sorter;
-    }
-
-    var sortOpts = {};
-    sortOpts[params.sort] = params.order;
-
-    self.fetchPromise.then(function () {
-        db.find(findOpts)
-            .sort(sortOpts)
-            .skip((filters.page - 1) * params.limit)
-            .limit(Number(params.limit))
-            .exec(function (err, docs) {
-                docs.forEach(function (entry) {
-                    entry.type = 'movie';
-                });
-
-                return defer.resolve({
-                    results: docs,
-                    hasMore: docs.length ? true : false
-                });
-            });
-    });
-
-    return defer.promise;
-};
-
-Vodo.prototype.detail = function (torrent_id, old_data) {
-    return Q(old_data);
-};
-
-
-module.exports = Vodo
